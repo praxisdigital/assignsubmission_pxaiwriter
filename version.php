4b15023a20064f20b2820f8e988d813bdebf66a3
Nilaksha <nilaksha.perera@ext.autotech.se>
2022-12-15 14:16:05 +0530

    no message

--- version.php
+++ version.php
@@ -5,4 +5,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2022121200;
 $plugin->requires   = 2018120302; // Moodle v3.6.2             
 $plugin->release    = 'v1.0.0.5';
-$plugin->component = 'assignsubmission_pxaiwriter';
+$plugin->component = 'submission_pxaiwriter';
