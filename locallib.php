054e1a2e00c01d754755205c30177a053af6f2b7
Nilaksha <nilaksha.perera@ext.autotech.se>
2023-01-07 08:16:52 +0530

    Fix first time pdf rendering issue on combining

--- locallib.php
+++ locallib.php
@@ -35,10 +35,6 @@ class assign_submission_pxaiwriter extends assign_submission_plugin
 
         $aiwritersteps = $this->get_config('pxaiwritersteps');
 
-
-        echo $this->getDiffRenderedHtml("Hello There! I am doing really good!", "Hello friend! I am doig very sad!");
-
-
         $stepList = array();
 
         if (!$aiwritersteps) {
@@ -178,15 +174,17 @@ class assign_submission_pxaiwriter extends assign_submission_plugin
 
         $pdf = new TCPDF();
         $pdf->AddPage();
-        $html = <<<EOD
-        <h1>Welcome 12345678901 to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+        $html = '<h1>Welcome 12345678901 to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
         <i>This is the first example of TCPDF library.</i>
         <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
         <p>Please check the source code documentation and other examples for further information.</p>
-        <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>
-        EOD;
+        <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>';
 
-        $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+        // $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+        $pdf->writeHTML($html, true, false, true, false, '');
+
+        $pdf->lastPage();
+
         $file = $pdf->Output($filename, 'S');
         //echo(var_dump($fileC));
 
