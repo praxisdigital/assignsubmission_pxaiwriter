a5d95d276e8ebcd239daa18ef10685e5624af78a
Nilaksha <nilaksha.perera@ext.autotech.se>
2023-01-10 10:42:25 +0530

    no message

--- lang/en/assignsubmission_pxaiwriter.php
+++ lang/en/assignsubmission_pxaiwriter.php
@@ -89,5 +89,5 @@ $string['open_ai_assignment_settings_description'] = "";
 
 $string["attempt_count"] = "Attempt count";
 
-$string["submission_due_msg"] = "Your assignment submission is due. You are restricted from using AI feature for this submission.";
+$string["submission_due_msg"] = "Your assignment submission is due. You are restricted from using the AI feature for this submission.";
 $string["ai_attempt_exceed_msg"] = "Your daily limit for using the AI feature is exceeded";
