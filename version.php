48d2944d415f63a425b3e332dd34c43577ee41f5
Sam Møller <41996743+sampraxis@users.noreply.github.com>
2023-04-04 11:22:28 +0200

    Update release version

--- version.php
+++ version.php
@@ -6,4 +6,4 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'assignsubmission_pxaiwriter';
 $plugin->version   = 2023040300;
 $plugin->requires   = 2018120302; // Moodle v3.6.2
-$plugin->release    = '1.0.1 (Build: 2023-03-30)';
+$plugin->release    = 'v1.1.0 (Build: 2023-04-03)';
