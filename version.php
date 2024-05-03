d65d9b9f4f6b96d8eba1f45a0899b450ade342a5
Sam Møller <smo@moxis.dk>
2024-05-03 12:41:08 +0200

    #26: OpenAI Chat completion max tokens issue
    Update version.php

--- version.php
+++ version.php
@@ -6,5 +6,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'assignsubmission_pxaiwriter';
 $plugin->version = 2024050100;
 $plugin->requires = 2021051700; // Moodle 3.11
-$plugin->release = '1.5.1 (Build: 2024-05-01)';
+$plugin->release = '1.6.0 (Build: 2024-05-01)';
 $plugin->maturity = MATURITY_STABLE;
